84cecceba4aa1f26801dafc22ebb22dd46c39473
AngelyThC <angelyth321@umes.edu.gt>
2025-02-13 11:51:47 -0600

    prueba tecnica

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoginButton  from "./components/LoginButton"; 
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* Aquí usamos el nuevo componente */}
+      <LoginButton />
+    </>
+  );
+}
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const container = document.getElementById('root');
+
+if (container) {
+  createRoot(container).render(
+    <Auth0Provider
+      domain="dev-liasfkyc8wblsigm.us.auth0.com"
+      clientId="u7QqtM0mKJI70glSevRG740xx9ENGmtK"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      <App />
+    </Auth0Provider>
+  );
+} else {
+  console.error("No se encontró el elemento con id 'root'");
+}
